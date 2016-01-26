ba990af1bf9041640299b8bdbf4bba2bcd26b168
Simskii <Simon@Simons-MacBook-Pro.local>
2016-01-26 17:09:04 +0100

    fix

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ var express = require('express'),
     http = require('http'),
     path = require('path');
 
-var port = process.env.port || 1339;
+var port = process.env.PORT || 1339;
 var server = express();
-
 var bodyParser = require('body-parser');
 
-
-
-server.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname + '/'));
 server.use(bodyParser.json());
 
 server.listen(port);
