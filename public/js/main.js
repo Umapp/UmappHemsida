554ef1442eece6a7e771bd5b0a320a4ed4373972
Simon Lundh <simon.lundh@live.se>
2016-03-18 12:57:43 +0100

    fi

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,45 @@
-$(document).ready(function(){
-    $('section[data-type="background"]').each(function(){
+$(document).ready(function() {
+    var mobile = false;
+
+    Modernizr.addTest('isios', function() {
+        return navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false
+    });
+
+    Modernizr.addTest('isandroid', function() {
+        return navigator.userAgent.match(/Android/i);
+    });
+    if (!Modernizr.isandroid) {
+        window.sr = ScrollReveal({ origin: 'bottom' });
+        sr.reveal('.title', { duration: 2000, origin: 'left', distance: '100px' });
+        sr.reveal('.reveal-right', { duration: 1600, origin: 'right', distance: '40px' });
+        sr.reveal('.reveal-left', { duration: 1600, origin: 'left', distance: '40px' });
+    }
+    if (Modernizr.isandroid) {
+        mobile = true;
+        $('.title').css('visibility', 'visible');
+    }
+
+    if (Modernizr.isios) {
+        mobile = true;
+        $('.banner').css('background-size', '800px auto');
+    }
+    
+    if(Modernizr.isios || Modernizr.isandroid){
+        $('.text-container').css('margin', '25px');
+    };
+
+    $('section[data-type="background"]').each(function() {
         var $bgobj = $(this); // assigning the object
         $window = $(window);
-
         $(window).scroll(function() {
-            var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+            if (!mobile) {
+                var yPos = -($window.scrollTop() / $bgobj.data('speed') * 2);
 
-            // Put together our final background position
-            var coords = '50% '+ yPos + 'px';
+                var coords = '50% ' + yPos + 'px';
+                $bgobj.css({ backgroundPosition: coords });
+            }
 
-            // Move the background
-            $bgobj.css({ backgroundPosition: coords });
+
         });
     });
 });
